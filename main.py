854d21ec4119cd2b9cfe965c06193eed75772377
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-11-27 10:51:07 +0000

    changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask, render_template, session
-from flask_bootstrap import Bootstrap5
+from bootstrap_flask import Bootstrap5
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
